a19810ee8a651bff0b23d6ef645628355e3cfa2c
aegurel <ahmetensarg01@gmail.com>
2022-12-21 17:19:57 +0300

    concat and copy

--- lodash.js
+++ lodash.js
@@ -9,9 +9,27 @@ function without(inp_arr, ...args){
     return inp_arr.filter(e=>!args.includes(e));
 }
 
+//the concat return an array that it is wanted to concatenate the arrays or values
+function concat(){
+
+}
+
+//the copyArray return an array that the copy of the source write to array
+function copyArray(source,array){
+    var i = -1;
+    length = source.length;
+
+    array || (array = Array(length));//if there is no array as parameter, we create empty array
+    while(++i < length){
+        array[i] = source[i];
+    }
+    return array;
+}
 
 
 module.exports = {
     compact:compact,
     without:without,
+    copyArray:copyArray,
+
 }
